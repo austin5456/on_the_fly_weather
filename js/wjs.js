0e2f0cae2938f7f09a7327022a42ade67f7500a3
austin5456 <austin1612@gmail.com>
2017-09-10 19:47:42 -0500

    styled loader, cleaned all code, and deleted unused files

--- js/wjs.js
+++ js/wjs.js
@@ -1,6 +1,5 @@
 ﻿"use strict";
 var global_wrap = document.getElementsByClassName("global_wrap")[0];
-
 var weatherConditions = new XMLHttpRequest();
 var weatherForecast = new XMLHttpRequest();
 var hourlyForecast = new XMLHttpRequest();
@@ -12,82 +11,59 @@ var state = document.getElementById("state");
 var current_weather = document.getElementById("current_weather");
 var current_temperature = document.getElementById("current_temperature");
 var current_weather_icon = document.getElementById("current_weather_icon");
-
 var timeZone;
-
 var loader = document.getElementsByClassName("loader")[0];
 var loadCheck = "";
-
 var today_month = document.getElementById("today_month");
 var zip_button = document.getElementById("zip_button");
 var zip_input = document.getElementById("zip_input");
-
-
 zip_button.addEventListener("click", loadWeather);
-
 
 function loadWeather(firstUseZip) {
     var zip = zip_input.value;
-
     if (!zip) {
         zip = firstUseZip
     }
-
     if (zip.length === 5 && !isNaN(zip)) {
         var current_condition_path = "http://api.wunderground.com/api/238e926ce0161f62/conditions/q/" + zip + ".json";
         var weekly_forecast_path = "http://api.wunderground.com/api/238e926ce0161f62/forecast10day/q/" + zip + ".json";
         var hourly_forecast_path = "http://api.wunderground.com/api/238e926ce0161f62/hourly/q/" + zip + ".json";
-
         weatherConditions.open("GET", current_condition_path, true);
         weatherConditions.responseType = "text";
         weatherConditions.send(null);
-
         weatherForecast.open("GET", weekly_forecast_path, true);
         weatherForecast.responseType = "text";
         weatherForecast.send(null);
-
         hourlyForecast.open("GET", hourly_forecast_path, true);
         hourlyForecast.responseType = "text";
         hourlyForecast.send(null);
         var thisPotato;
-    }
-    else {
+    } else {
         console.log("nice try buddy");
     }
     zip_input.value = "";
 }
-
 weatherConditions.onload = function () {
     if (weatherConditions.status === 200) {
         cObj = JSON.parse(weatherConditions.responseText);
         console.log(cObj);
         city.textContent = cObj.current_observation.display_location.city;
         state.textContent = cObj.current_observation.display_location.state_name;
-
-
-
         imLoading("Y");
     }
 }
-
-
-
 weatherForecast.onload = function () {
     if (weatherForecast.status === 200) {
         fObj = JSON.parse(weatherForecast.responseText);
         console.log(fObj);
         timeZone = fObj.forecast.simpleforecast.forecastday["0"].date.tz_short;
-
         var wk_forecast_day_text = document.getElementsByClassName("wk_forecast_day_text");
         var wk_forecast_day_date = document.getElementsByClassName("wk_forecast_day_date");
         var wk_forecast_temp_high = document.getElementsByClassName("wk_forecast_temp_high");
         var wk_forecast_temp_low = document.getElementsByClassName("wk_forecast_temp_low");
         var wk_forecast_icon = document.getElementsByClassName("weather_icon_wrap");
         var wk_forecast_condition = document.getElementsByClassName("wk_forecast_condition");
-
-
         for (var i = 0; i < 7; i++) {
-
             wk_forecast_day_text[i].textContent = fObj.forecast.simpleforecast.forecastday[i].date.weekday_short;
             wk_forecast_day_date[i].textContent = fObj.forecast.simpleforecast.forecastday[i].date.day;
             wk_forecast_temp_high[i].textContent = "High " + fObj.forecast.simpleforecast.forecastday[i].high.fahrenheit + "°f";
@@ -95,46 +71,35 @@ weatherForecast.onload = function () {
             iconSwapper(wk_forecast_icon[i].children[0], fObj.forecast.simpleforecast.forecastday[i].icon_url);
             wk_forecast_condition[i].textContent = fObj.forecast.simpleforecast.forecastday[i].conditions;
         }
-
         //for the clock
         today_month.textContent = fObj.forecast.simpleforecast.forecastday["0"].date.monthname;
-
         imLoading("Y");
-
     }
 }
-
-
 hourlyForecast.onload = function () {
     if (hourlyForecast.status === 200) {
         hObj = JSON.parse(hourlyForecast.responseText);
         console.log(hObj);
-
         //using for current condition, more accurate
         iconSwapper(current_weather_icon, hObj.hourly_forecast["0"].icon_url);
         today_month.textContent = hObj.hourly_forecast["0"].FCTTIME.month_name;
         current_weather.textContent = hObj.hourly_forecast["0"].condition;
         current_temperature.textContent = hObj.hourly_forecast["0"].temp.english;
         //end current conditions
-
-
         var myHourlyVariables = {
             icons: "hourly_icon_",
             temperatures: "hourly_temp_"
         }
-
         for (var i = 0; i < 9; i++) {
             window[myHourlyVariables.icons + i] = document.getElementById(myHourlyVariables.icons + i);
             window[myHourlyVariables.temperatures + i] = document.getElementById(myHourlyVariables.temperatures + i);
         }
-
         for (var i = 0; i < 9; i++) {
             iconSwapper(window["hourly_icon_" + i], hObj.hourly_forecast[i * 3].icon_url);
             window["hourly_temp_" + i].textContent = hObj.hourly_forecast[i * 3].temp.english + "°";
         }
 
         function dayNightPosition() {
-
             var positionArray = ["30px", "45px", "45px", "30px", "15px", "0", "0", "15px", "30px", "45px", "45px", "30px", "15px", "0", "0", "15px", "30px"];
             var dayOrNight;
             var positionString = "";
@@ -142,22 +107,26 @@ hourlyForecast.onload = function () {
                 // if in day hours
                 if (Number(hObj.hourly_forecast[(i * 3)].FCTTIME.hour) >= 8 && Number(hObj.hourly_forecast[(i * 3)].FCTTIME.hour) <= 19) {
                     dayOrNight = "D";
-                }
-                else {
+                } else {
                     dayOrNight = "N";
                 }
                 positionString += dayOrNight;
             }
-
-            var sun_moon = document.getElementsByClassName("sun")[0];
-
+            var sun_moon = document.getElementById("sun_moon");
             if (positionString.charAt(0) === "N") {
                 global_wrap.className = "global_wrap night_sky"
                 sun_moon.className = "moon";
             }
-
-            var num = { NNNN: 0, NNND: 1, NNDD: 2, NDDD: 3, DDDD: 4, DDDN: 5, DDNN: 6, DNNN: 7 }[positionString];
-
+            var num = {
+                NNNN: 0,
+                NNND: 1,
+                NNDD: 2,
+                NDDD: 3,
+                DDDD: 4,
+                DDDN: 5,
+                DDNN: 6,
+                DNNN: 7
+            }[positionString];
             if (window.matchMedia("(min-width: 1000px)").matches) {
                 for (var i = 0 + num; i < 9 + num; i++) {
                     window["hourly_icon_" + (i - num)].style.top = positionArray[i];
@@ -172,14 +141,11 @@ hourlyForecast.onload = function () {
             document.getElementsByTagName("body")[0].onresize = function () {
                 dayNightPosition();
             }
-
         }
         dayNightPosition();
-
         imLoading("Y");
     }
 }
-
 var pHours = document.getElementById("hours"),
     pMinutes = document.getElementById("minutes"),
     pSeconds = document.getElementById("seconds"),
@@ -187,28 +153,22 @@ var pHours = document.getElementById("hours"),
     ampm;
 
 function setTime() {
-
     var date = new Date();
     updateSeconds(date.getSeconds());
     updateMinute(date.getMinutes());
     updateHour(date.getHours());
-
     var update_date = function () {
-
         date = new Date();
         var seconds = date.getSeconds();
         updateSeconds(seconds);
-
         if (seconds === 0) {
             var minutes = date.getMinutes();
             updateMinute(minutes);
         }
-
         if (minutes === 0 && seconds === 0) {
             var hours = date.getHours();
             updateHour(hours)
         }
-
     }
 
     function updateSeconds(seconds) {
@@ -234,14 +194,12 @@ function setTime() {
         } else {
             ampm = "AM";
         }
-
         if (hours === 0) {
             hours = 12;
         }
         pampm.textContent = ampm;
         pHours.textContent = hours;
     }
-
     update_date();
     setInterval(update_date, 1000);
 }
@@ -266,11 +224,9 @@ var icons = {
     nt_rain: "-3% 20%"
 }
 //sets the icons (when called upon)
-
-
 function iconSwapper(element, feed) {
     //console.log(feed); check which is wrong
-    switch(feed) {
+    switch (feed) {
         case "http://icons.wxug.com/i/c/k/partlycloudy.gif":
             element.style.backgroundPosition = icons.day_partlycloudy;
             break;
@@ -324,13 +280,9 @@ function iconSwapper(element, feed) {
             break;
     }
 }
-
-
 $(document).ready(function () {
     console.log("ready!");
-
 });
-
 loadWeather("76148");
 
 function imLoading(didILoad) {
@@ -338,7 +290,6 @@ function imLoading(didILoad) {
     var myBody = document.getElementsByTagName("body")[0];
     var loader_wrapper = document.getElementById("loader_wrapper");
     if (loadCheck === "YYY") {
-
         console.log("done Loading");
         setTimeout(function () {
             myBody.className = "loaded";
@@ -346,8 +297,7 @@ function imLoading(didILoad) {
                 loader_wrapper.style.display = "none";
             }, 1500);
         }, 150);
-    }
-    else {
+    } else {
         console.log("still Loading");
     }
 }
